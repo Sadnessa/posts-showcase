dc21d9b490ae32ba53a4aacde481d0e23470f0b0
Darya <kequeue.8@gmail.com>
2024-03-08 11:12:26 +0200

    Added style changes to PublicationCard component.

--- src/components/PublicationCard.vue
+++ src/components/PublicationCard.vue
@@ -1,16 +1,20 @@
 <template>
   <div
-    class="publicationCard flex flex-col gap-2.5 shadow-sm bg-white rounded p-4 border"
+    class="publicationCard flex flex-col gap-2.5 shadow-sm bg-white rounded p-4 py-5 border"
   >
     <div
-      class="publicationCard__title text-sky-500 font-semibold first-letter:capitalize"
+      class="publicationCard__title text-sky-500 text-xl first-letter:capitalize font-semibold"
     >
       {{ props.title }}
     </div>
-    <div class="publicationCard__content first-letter:capitalize">
+    <div
+      class="publicationCard__content first-letter:capitalize text-neutral-900"
+    >
       {{ props.content }}
     </div>
-    <div class="publicationCard__author first-letter:capitalize">
+    <div
+      class="publicationCard__author first-letter:capitalize grow flex items-end text-gray-400 text-sm"
+    >
       {{ props.author }}
     </div>
   </div>
